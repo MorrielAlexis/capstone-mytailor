ec321c434d7e51a2c4503675a9a55d152408a8be
Morriel Aquino <bimbsaquino090409@gmail.com>
2016-04-20 12:52:32 +0800

    table for materials

---
+++ database/migrations/2016_04_20_043620_create_thread_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tblMaterialThread', function (Blueprint $table) {
+            $table->string('strMaterialThreadID')->primary();
+            $table->string('strMaterialThreadName');
+            $table->string('strMaterialThreadColor');
+            $table->string('strMaterialThreadDesc');
+            $table->string('strMaterialThreadImage');
+            $table->boolean('boolIsActive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tblMaterialThread');
+    }
+}
