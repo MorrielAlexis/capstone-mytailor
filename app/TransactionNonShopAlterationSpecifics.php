91985378ebe1ce9c0a06cd87a493b468ca9c8907
Daniel Valle <israelvalle48@gmail.com>
2016-07-23 14:01:26 +0800

    alteration

---
+++ app/TransactionNonShopAlterationSpecifics.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionNonShopAlterationSpecifics extends Model
+{
+    public $table = 'tblNonShopAlterSpecific';
+
+    public $primaryKey = 'strNonAlterSpecificID';
+
+    public $fillable = array('strNonAlterSpecificID', 
+    						'strNonShopAlterFK',
+    						'strGarmentSegmentFK');
+}
