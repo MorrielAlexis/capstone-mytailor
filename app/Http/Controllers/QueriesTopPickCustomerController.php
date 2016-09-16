d513fe4e03536ff2829dc5d276d21b33e67c46e0
Morriel Aquino <bimbsaquino090409@gmail.com>
2016-09-16 18:27:52 +0800

    initial for queries top customer

--- app/Http/Controllers/QueriesTopPickCustomerController.php
+++ app/Http/Controllers/QueriesTopPickCustomerController.php
@@ -16,8 +16,8 @@ class QueriesTopPickCustomerController extends Controller
      */
     public function index()
     {
-        $topCustomers = \DB::select("SELECT Concat(tblCustIndividual.strIndivFName, '' , tblCustIndividual.strIndivMName, '' , tblCustIndividual.strIndivMName)as name, COUNT(strIndivID) as ctr FROM tblJobOrder,tblCustIndividual WHERE tblCustIndividual.strIndivID = tblJobOrder.strJO_CustomerFK GROUP BY strIndivID ORDER BY ctr DESC limit 3");
-        dd($topCustomers);
+        $topCustomers = \DB::select("SELECT Concat(tblCustIndividual.strIndivFName, '' , tblCustIndividual.strIndivMName, '' , tblCustIndividual.strIndivLName)as name, COUNT(strIndivID) as ctr FROM tblJobOrder,tblCustIndividual WHERE tblCustIndividual.strIndivID = tblJobOrder.strJO_CustomerFK GROUP BY strIndivID ORDER BY ctr DESC limit 3");
+        // dd($topCustomers);
 
         return view('queries.queries-top-customers')
         ->with('topCustomers', $topCustomers);
--- resources/views/queries/queries-top-customers.blade.php
+++ resources/views/queries/queries-top-customers.blade.php
@@ -26,7 +26,7 @@
               <thead>
                   <tr>
                     <!--<th data-field="id">Garment ID</th>-->
-                    <th data-field="CustID">Customer ID</th>
+                    {{-- <th data-field="CustID">Customer ID</th> --}}
                     <th data-field="Customer Name">Customer Name</th>
                     <th data-field="Total Orders">Total Orders</th>
                     <!-- <th>Deactivate</th> -->
@@ -37,9 +37,9 @@
                     @foreach($topCustomers as $topCustomer)
                     {{--  @if($topCustomer->boolIsActive == 1) --}}
                   <tr>
-                    <td>{{$topCustomer->strIndivID}}</td>
-                    <td>{{$topCustomer->strIndivFName}} {{$topCustomer->strIndivMName}} {{$topCustomer->strIndivLName}}</td>
-                    <td></td> 
+                    {{-- <td>{{$topCustomer->strIndivID}}</td> --}}
+                    <td>{{$topCustomer->name}}</td>
+                    <td>{{$topCustomer->ctr}}</td> 
                   </tr>
                 {{--   @endif --}}
                   @endforeach
