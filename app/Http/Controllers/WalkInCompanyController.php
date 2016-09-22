64991bf6eb74433aeafc66d5ae6575760d9a2def
aryangreys <arianne_spice@yahoo.com>
2016-09-23 02:58:01 +0800

    sorry Dan haha di ko napansin eh huhu

--- app/Http/Controllers/WalkInCompanyController.php
+++ app/Http/Controllers/WalkInCompanyController.php
@@ -950,8 +950,6 @@ class WalkInCompanyController extends Controller
 
     }//end of job order
 
-<<<<<<< HEAD
-=======
     public function removePackage(Request $request)
     {
         $to_be_deleted = ((int)$request->input('hidden_remove_package'));
@@ -1013,7 +1011,6 @@ class WalkInCompanyController extends Controller
         session()->forget('payment_id');
     }
 
->>>>>>> 69d556dec8db90b2871dad1bb0b41826db095ac5
     /*For downloadable forms*/
     public function downloadForms()
     {
